f6f22df2cf6db689c9ab8daa803e21c04f6f9d86
fuad221 <fuad221>
2021-10-20 20:09:04 +0200

    i created Context's file and i added the some data

---
+++ src/Context.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const NotesContext = createContext({
+    currentNote: null,
+    notes: [
+        { id: 1, text: 'Do homework' },
+        { id: 2, text: 'Take out trash' },
+        { id: 3, text: 'Clean room' }
+      ]
+    });
+    export default NotesContext;
+
+
+
