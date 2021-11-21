fe1ac7a1ca91cf03c8071f5415ae3ecaf9acf7a7
fuad Abdullah <69249087+fuad221@users.noreply.github.com>
2021-11-21 11:50:32 +0100

    Add files via upload
    I shortened the export codes.

---
+++ src/Reference-import.js
@@ -0,0 +1,23 @@
+export { default as  context } from './context';
+export { default as  Reducer } from './Reducer';
+export { default as  TasksContext } from './context';
+export { default as  TasksReducer } from './Reducer';
+
+
+export { default as  AddTask } from './components/AddTask';
+export { default as  EditTask } from './components/EditTask';
+export { default as  Footer } from './components/Footer';
+export { default as  Header } from './components/Header';
+export { default as  Task } from './components/Task';
+export { default as  TaskList } from './components/TaskList';
+
+
+
+export { default as FooterCSS } from './components/Footer.css';
+export { default as TaskCss } from './components/Task.css';
+
+
+
+
+
+
